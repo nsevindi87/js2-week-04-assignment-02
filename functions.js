0a6e86e61ed724833de74189a3da2d54e73752c0
Nizami Sevindi <nsevindi87@gmail.com>
2022-12-03 23:31:17 +0100

    week 4 assignment 2

--- functions.js
+++ functions.js
@@ -1,3 +1,14 @@
-const isTheWordSplit = () => {};
+const isTheWordSplit = (pWordArr) => {
+    let firstElement = pWordArr[0]
+    let secondElement = pWordArr[1].split(",")
+    for (let index = 0; index < secondElement.length; index++) {
+        for (let j = 0; j < secondElement.length; j++) {
+            if(secondElement[index] + secondElement[j] === firstElement){
+                return `${secondElement[index]},${secondElement[j]}`
+            }
+        }
+    }
+    return "not possible"
+};
 
 export { isTheWordSplit };
